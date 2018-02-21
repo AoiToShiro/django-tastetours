7ce9df7fc58690ed58cbbe188b1d54a713783236
AoiToShiro <kevinbenneworth@gmail.com>
2018-02-21 12:32:28 +0000

    commented out Anymail settings as confusing to Heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,7 +62,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
-    'anymail',
+    # 'anymail', duplicate to production settings
 ]
 
 # Apps specific for this project go here.
