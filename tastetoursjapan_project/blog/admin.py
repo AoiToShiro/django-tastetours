24e188620f93647916857d9e9dd86c70c0e58070
AoiToShiro <kevinbenneworth@gmail.com>
2018-04-13 11:03:29 +0100

    added django-mediumeditor to allow basic formatting in blog

--- tastetoursjapan_project/blog/admin.py
+++ tastetoursjapan_project/blog/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+from mediumeditor.admin import MediumEditorAdmin
 
-
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(MediumEditorAdmin, admin.ModelAdmin):
     list_display = ('title','slug','author','status','created')
     list_filter = ('status','created','publish','author')
     search_fields = ('title','author')
@@ -13,5 +13,6 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+    mediumeditor_fields = ('body')
 
 admin.site.register(Post, PostAdmin)
