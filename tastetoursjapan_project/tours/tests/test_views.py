decf0434fd3b96a6552ecd7b94206e2561db826e
AoiToShiro <kevinbenneworth@gmail.com>
2018-09-11 16:55:59 +0100

    Not yet finished with creating new tour. Unit tests fail for html template incorrectly

--- tastetoursjapan_project/tours/tests/test_views.py
+++ tastetoursjapan_project/tours/tests/test_views.py
@@ -1,64 +1,43 @@
-from django.test import RequestFactory
-
+# from django.test import RequestFactory
+# from django.test import TestCase
 from test_plus.test import TestCase
-# 
-# from ..views import (
-#     UserRedirectView,
-#     UserUpdateView
-# )
-#
-#
-# class BaseUserTestCase(TestCase):
-#
-#     def setUp(self):
-#         self.user = self.make_user()
-#         self.factory = RequestFactory()
-#
-#
-# class TestUserRedirectView(BaseUserTestCase):
-#
-#     def test_get_redirect_url(self):
-#         # Instantiate the view directly. Never do this outside a test!
-#         view = UserRedirectView()
-#         # Generate a fake request
-#         request = self.factory.get('/fake-url')
-#         # Attach the user to the request
-#         request.user = self.user
-#         # Attach the request to the view
-#         view.request = request
-#         # Expect: '/users/testuser/', as that is the default username for
-#         #   self.make_user()
-#         self.assertEqual(
-#             view.get_redirect_url(),
-#             '/users/testuser/'
-#         )
-#
-#
-# class TestUserUpdateView(BaseUserTestCase):
-#
-#     def setUp(self):
-#         # call BaseUserTestCase.setUp()
-#         super(TestUserUpdateView, self).setUp()
-#         # Instantiate the view directly. Never do this outside a test!
-#         self.view = UserUpdateView()
-#         # Generate a fake request
-#         request = self.factory.get('/fake-url')
-#         # Attach the user to the request
-#         request.user = self.user
-#         # Attach the request to the view
-#         self.view.request = request
-#
-#     def test_get_success_url(self):
-#         # Expect: '/users/testuser/', as that is the default username for
-#         #   self.make_user()
-#         self.assertEqual(
-#             self.view.get_success_url(),
-#             '/users/testuser/'
-#         )
-#
-#     def test_get_object(self):
-#         # Expect: self.user, as that is the request's user object
-#         self.assertEqual(
-#             self.view.get_object(),
-#             self.user
-#         )
+from django.http import HttpRequest
+from tours.views import (
+    edooutpost, sumofireizakaya, tours
+)
+
+class HomePageTest(TestCase):
+
+    def test_home_page_live(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get(f'/')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class EdoTestCase(TestCase):
+
+    def test_edo_page_live(self):
+        response = self.client.get('/tours/edooutpost/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edo_tour_returns_correct_html(self):
+        response = self.client.get(f'/tours/edooutpost/')
+        self.assertTemplateUsed(response, 'edooutpost.html')
+
+    def test_access_from_home_page(self):
+        """Test that the link in the home page goes to the correct page"""
+        # setup css id for the drop down menu: navbarDropdownMenuLink
+        self.fail()
+
+class SumoTestCase(TestCase):
+
+    def test_edo_page_live(self):
+        response = self.client.get('/tours/sumofireizakaya/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edo_tour_returns_correct_html(self):
+        response = self.client.get(f'/tours/sumofireizakaya/')
+        self.assertTemplateUsed(response, 'sumofireizakaya.html')
